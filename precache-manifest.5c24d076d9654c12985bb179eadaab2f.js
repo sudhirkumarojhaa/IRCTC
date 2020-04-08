4c8192bf3c15051a1c5f29823d41aa632402cae7
James Sudhir <jamessudhir@Sudhir-MacBook-Air.local>
2020-04-09 02:27:55 +0530

    Updates

---
+++ precache-manifest.5c24d076d9654c12985bb179eadaab2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7fccb22e7b70524659c33f5f8f7a123",
+    "url": "/IRCTC/index.html"
+  },
+  {
+    "revision": "e91dae9fbd9c157656f4",
+    "url": "/IRCTC/static/css/main.1662d647.chunk.css"
+  },
+  {
+    "revision": "f884bd657a65be357be5",
+    "url": "/IRCTC/static/js/2.25ab35e1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/IRCTC/static/js/2.25ab35e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91dae9fbd9c157656f4",
+    "url": "/IRCTC/static/js/main.b4433a8f.chunk.js"
+  },
+  {
+    "revision": "8108d36c86813a446852",
+    "url": "/IRCTC/static/js/runtime-main.849e550a.js"
+  },
+  {
+    "revision": "c8baa5674202d777c89ae0105466e662",
+    "url": "/IRCTC/static/media/inr.c8baa567.png"
+  }
+]);
